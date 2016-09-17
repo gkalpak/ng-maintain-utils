1642a015b5e609cc5080f3d9f3a9de988d0af853
Georgios Kalpakas <kalpakas.g@gmail.com>
2016-09-17 23:59:13 +0300

    test(index): add tests for `index`

---
+++ test/unit/index.spec.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Imports - Local
+let index = require('../../index');
+
+// Tests
+describe('index', () => {
+  it('should expose all necessary classes', () => {
+    expect(index).toEqual({
+      AbstractCli: jasmine.any(Function),
+      ArgSpec: jasmine.any(Function),
+      CleanUper: jasmine.any(Function),
+      Config: jasmine.any(Function),
+      GitUtils: jasmine.any(Function),
+      Phase: jasmine.any(Function),
+      UiUtils: jasmine.any(Function),
+      Utils: jasmine.any(Function)
+    });
+  });
+});
